11f99be8cd0932445b4bf0a4f94d5c298ed43aaa
Space Pirate <b22ee065@iitj.ac.in>
2023-12-12 23:17:55 +0530

    no idea

--- build/CMakeFiles/path_plan.dir/DependInfo.cmake
+++ build/CMakeFiles/path_plan.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/rakshit/Desktop/path/src/main.cpp" "/home/rakshit/Desktop/path/build/CMakeFiles/path_plan.dir/src/main.cpp.o"
+  "/home/rakshit/Desktop/Path/src/main.cpp" "/home/rakshit/Desktop/Path/build/CMakeFiles/path_plan.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
